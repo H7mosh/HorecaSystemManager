0d2e45f6012a38544c67a920d0ac5a6a32f64fe4
Muhammed Amin <muhammed.amin.coder@gmail.com>
2024-04-17 09:32:41 +0300

    p.shtiwa.n.

--- sacmy/Client/Program.cs
+++ sacmy/Client/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using MudBlazor.Services;
 using sacmy.Client;
+using sacmy.Client.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +14,7 @@ builder.Services.AddHttpClient("sacmy.ServerAPI", client => client.BaseAddress =
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("sacmy.ServerAPI"));
+builder.Services.AddScoped<PurchaseService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
